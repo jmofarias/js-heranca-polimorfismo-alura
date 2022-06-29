e3dacd0319cbde012b0181b4b610c77605ef07ff
joaomarcos <jjmarcos18@hotmail.com>
2022-06-29 20:51:33 -0300

    first commit

---
+++ Conta.js
@@ -0,0 +1,31 @@
+export class Conta {
+    constructor(saldoInicial, cliente, agencia){
+        this._saldo = saldoInicial;
+        this._cliente = cliente;
+        this._agencia = agencia;
+    }
+
+    sacar(valor){
+        // ao sacar de uma conta corrente será cobrado uma taxa de 10%
+        taxa = 1.1 * valor; 
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0)
+        {
+            return;
+        } 
+        this._saldo += valor;           
+    }
+
+    tranferir(valor, conta){
+        
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+<h1 align="center">
+    <img alt="Logo-Dev-Club" src="./img/capa-alura-mesha.png" width = 450px>
+</h1>
+
+<p align="center">
+  <img alt="JavaScript" src="https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E"/>
+</p>
+
+## :computer: Sobre o curso
+Através do curso foi possível aprender:
+
+- DuckType
+- Herança
+- Boas práticas
